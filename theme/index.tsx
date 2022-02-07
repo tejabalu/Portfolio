a2fc4a7040b26f36eca16298d8dbb0772aaeace9
tejabalu <tejabalu15@gmail.com>
2022-02-07 09:50:31 -0800

    default dark mode

--- theme/index.tsx
+++ theme/index.tsx
@@ -2,17 +2,18 @@ import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
-  useSystemColorMode: true,
+	useSystemColorMode: true,
+	initialColorMode: "dark",
 };
 
 const styles = {
-  global: (props) => ({
-    body: {
-      bg: mode("gray.50", "#151a23")(props),
-    },
-  }),
+	global: (props) => ({
+		body: {
+			bg: mode("gray.50", "#151a23")(props),
+		},
+	}),
 };
 
-const theme = extendTheme({ ...config, styles });
+const theme = extendTheme({ config, styles });
 
 export default theme;
