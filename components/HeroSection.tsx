28c45acfd742a85bd1863c03a4b55978cc275533
tejabalu <tejabalu15@gmail.com>
2022-03-20 20:21:03 -0700

    3D Section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,11 +5,11 @@ import React from "react";
 const HeroSection = () => {
 	return (
 		<SlideFade in={true} offsetY={80}>
-			<Box marginTop={"2em"}>
+			<Box marginTop={"2em"} overflow={"hidden"}>
 				<iframe
 					src="https://my.spline.design/miniroomcopy-ad8331c62ca1a1cd3cf73e143cf03212/"
 					frameBorder="0"
-					width="100%"
+					width="120%"
 					height="400em"
 				></iframe>
 			</Box>
