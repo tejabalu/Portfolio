25b9ab5c593abbc54fb73ec31bce497b6d86ccf6
tejabalu15 <tejabalu15@gmail.com>
2022-12-14 02:13:31 -0800

    link wrap

--- components/pages/index/Profile.tsx
+++ components/pages/index/Profile.tsx
@@ -6,6 +6,7 @@ import {
   LightMode,
   Link,
   SlideFade,
+  Wrap,
 } from "@chakra-ui/react";
 
 import { FaEnvelope, FaFileAlt, FaGithub, FaLinkedin } from "react-icons/fa";
@@ -40,53 +41,55 @@ const Profile = () => {
         <Box mt={10}>
           <LightMode>
             <ButtonGroup>
-              <Button
-                colorScheme="blue"
-                bg="blue.500"
-                href="mailto:tejabalu15@gmail.com"
-                borderRadius={5}
-                p={3}
-                as="a"
-                leftIcon={<FaEnvelope />}
-              >
-                tejabalu15@gmail.com
-              </Button>
-              <Button
-                colorScheme="blue"
-                bg="blue.500"
-                href="https://docs.google.com/document/d/1UKAAynX2A7kP8DkbG_PR8139wjh6NND-RjCdJuLtzeU/edit#heading=h.1nyyawuw8ykd"
-                target={`_blank`}
-                borderRadius={5}
-                p={3}
-                as="a"
-                leftIcon={<FaFileAlt />}
-              >
-                Resume
-              </Button>
-              <Button
-                colorScheme="blue"
-                bg="blue.500"
-                href="https://www.linkedin.com/in/tejabalu/"
-                target={`_blank`}
-                borderRadius={5}
-                p={3}
-                as="a"
-                leftIcon={<FaLinkedin />}
-              >
-                LinkedIn
-              </Button>
-              <Button
-                colorScheme="blue"
-                bg="blue.500"
-                href="https://github.com/tejabalu/"
-                target={`_blank`}
-                borderRadius={5}
-                p={3}
-                as="a"
-                leftIcon={<FaGithub />}
-              >
-                GitHub
-              </Button>
+              <Wrap>
+                <Button
+                  colorScheme="blue"
+                  bg="blue.500"
+                  href="mailto:tejabalu15@gmail.com"
+                  borderRadius={5}
+                  p={3}
+                  as="a"
+                  leftIcon={<FaEnvelope />}
+                >
+                  tejabalu15@gmail.com
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  bg="blue.500"
+                  href="https://docs.google.com/document/d/1UKAAynX2A7kP8DkbG_PR8139wjh6NND-RjCdJuLtzeU/edit#heading=h.1nyyawuw8ykd"
+                  target={`_blank`}
+                  borderRadius={5}
+                  p={3}
+                  as="a"
+                  leftIcon={<FaFileAlt />}
+                >
+                  Resume
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  bg="blue.500"
+                  href="https://www.linkedin.com/in/tejabalu/"
+                  target={`_blank`}
+                  borderRadius={5}
+                  p={3}
+                  as="a"
+                  leftIcon={<FaLinkedin />}
+                >
+                  LinkedIn
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  bg="blue.500"
+                  href="https://github.com/tejabalu/"
+                  target={`_blank`}
+                  borderRadius={5}
+                  p={3}
+                  as="a"
+                  leftIcon={<FaGithub />}
+                >
+                  GitHub
+                </Button>
+              </Wrap>
             </ButtonGroup>
           </LightMode>
         </Box>
