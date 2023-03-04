9c3752129f80ebec2d09c254b333aa60aa6b2a0e
Teja Balu <tejabalu15@gmail.com>
2023-03-04 13:35:30 -0800

    Implemented loaded spinner for spline component.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,9 +1,8 @@
 import { Box, Center, SlideFade, Spinner } from "@chakra-ui/react";
-import Spline from "@splinetool/react-spline";
 import { useState } from "react";
 
 const HeroSection = () => {
-  const [loading, setLoading] = useState("none");
+  const [loading, setLoading] = useState(true);
 
   return (
     <SlideFade in={true} offsetY={80}>
@@ -16,26 +15,28 @@ const HeroSection = () => {
           marginTop={"-2em"}
           width={["100%", "100%", "80%", "80%"]}
         >
-          <Box width={"130%"} height={"100%"} style={{ marginLeft: "-15%" }}>
-            {loading && <Spinner zIndex={10} />}
-            <Spline
-              scene="https://prod.spline.design/IsqEF9i7tPadTQFz/scene.splinecode"
-              onLoad={() => {
-                console.log("loaded");
-                setLoading("block");
-              }}
-            />
-          </Box>
-
-          {/* <iframe
+          {loading && (
+            <Center
+              width={"100%"}
+              height={"100%"}
+              zIndex={-999}
+              position={"relative"}
+            >
+              <Spinner />
+            </Center>
+          )}
+          <iframe
             src="https://my.spline.design/glasstypecopy-7299a9cefc82e907fffed6caed00ae11/"
-            // frameBorder="0"
             width="130%"
-            height="400em"
+            height="100%"
             style={{
               marginLeft: "-15%",
+              zIndex: 999,
             }}
-          ></iframe> */}
+            onLoad={() => {
+              setLoading(false);
+            }}
+          ></iframe>
         </Box>
       </Center>
     </SlideFade>
--- components/SplineComponent.tsx
+++
@@ -1,6 +0,0 @@
-
-export function SplineComponent(setLoading) {
-  return (
-
-  );
-}
